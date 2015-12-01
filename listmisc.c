5ec7be03a6da69856dbcff4726e069db2837d138
Philipp Ludwig <git@philippludwig.net>
2015-12-01 18:36:10 +0100

    Added more documentation.

--- listmisc.c
+++ listmisc.c
@@ -1,3 +1,29 @@
+/* liblistmisc source file.
+ *
+ * Copyright (c) 2013-2015 Philipp Ludwig <git@philippludwig.net>
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "listmisc.h"
 
 #include <stdbool.h>
@@ -14,10 +40,19 @@ bool list_add(list_t *list, void *data)
 
 	// Create new node.
 	list_t *node = calloc(1, sizeof(list_t));
+
+	// Assign the data.
 	node->d = data;
 
 	// Add node.
+	// We do this by updating the next pointer of the last node
+	// to point to the newly created node.
 	list->end->next = node;
+
+	// Furthermore we update the end pointer of the list item.
+	// TODO: Maybe we should update the end pointer of every list item.
+	// This could by handled by setting node->prev->end = node,
+	// but this hasn't been tested yet.
 	list->end = node;
 
 	// Success.
@@ -26,7 +61,8 @@ bool list_add(list_t *list, void *data)
 
 void* list_remove(list_t *list, const void *data)
 {
-	if(!list || !data) return 0;
+	// Check for a valid list first.
+	if(!list) return 0;
 
 	// Find the corresponding list element.
 	struct list_t *prev = list;
@@ -43,7 +79,10 @@ void* list_remove(list_t *list, const void *data)
 		// Update the end pointer if needed.
 		if(list->end == it) list->end = prev;
 
+		// Free the memory assigned to the removed node.
 		free(it);
+
+		// Return the pointer to the data.
 		return ret;
 	}
 
@@ -58,25 +97,40 @@ list_t* list_start(const list_t *list)
 
 list_t* list_create()
 {
+	// Allocate memory for the new list.
 	list_t *list = calloc(1, sizeof(list_t));
+
+	// Return 0 if the memory could not be allocated.
 	if(!list) return 0;
+
+	// Initialize the end pointer.
 	list->end = list;
+
+	// Return the new created list.
 	return list;
 }
 
 bool list_empty(const list_t *list)
 {
-	if(!list) return 1;
-	if(!list->next) return 1;
-	return 0;
+	// Invalid lists are empty lists.
+	if(!list) return true;
+
+	// Lists without a next node are empty list-
+	if(!list->next) return true;
+
+	// In other cases this list is not empty.
+	return false;
 }
 
 void list_free(list_t *list)
 {
+	// Iterate over all items in the list.
 	for(list_t *it = list; it != 0; ) {
-		if(it->d) free(it->d);
+		// If the data pointer is not a nullptr, we free it.
+		if(it->d != 0) free(it->d);
 		it->d = 0;
 
+		// We free this node and set our iterator to the next item.
 		list_t *tmp = it;
 		it = it->next;
 		free(tmp);
--- listmisc.h
+++ listmisc.h
@@ -1,9 +1,53 @@
 #ifndef LIST_H_
 #define LIST_H_
 
+/* liblistmisc header file 
+ *
+ * 
+ * Copyright (c) 2013-2015 Philipp Ludwig <git@philippludwig.net>
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+// This header is just included for the return type for some methods.
+// If you prefer do use as little standard library features as possible,
+// just #define bool, true and false yourself.
 #include <stdbool.h>
 
-/** List structure. **/
+/**
+ * @brief List structure.
+ *
+ * This is the structure of a list node.
+ * next points to the next list item, end points always to the last
+ * item in the list.
+ * d points to the data item transferred with list_add.
+ *
+ * Please note that the very first item of every list is not
+ * a valid list node, that means it does not hold any data.
+ *
+ * This approach was chosen to allow "empty" lists.
+ *
+ * Refer to the file "example.c" or "README.md" for usage instructions.
+ **/
 typedef struct list_t {
 	/** Data that this node contains, can be anything. **/
 	void *d;
@@ -23,6 +67,16 @@ void list_free(list_t *list);
 
 /**
  * @brief Add some data to the given list.
+ * @param list A pointer to a list_t element.
+ * @param data A pointer to the data that will be hold by the new list element.
+ *
+ * Note: The ownership of the pointer is transferred to the new list item.
+ * Therefore, data must not be freed after list_add has been called.
+ *
+ * If you want to free the data after it has been added to the list,
+ * call list_remove first!
+ *
+ * @sa list_remove
  * @return true: success, false: failure.
  */
 bool list_add(list_t *list, void *data);
@@ -39,7 +93,11 @@ bool list_empty(const list_t *list);
  ***/
 void* list_remove(list_t *list, const void *data);
 
+// The list_foreach macro depends on C99 support / recent versions of MSVC.
 #if (__STDC_VERSION__ >= 199901L) || (_MSC_VER >= 1800)
+/**
+ * @brief Handy macro which makes it easier to iterate over a list.
+ */
 #define list_foreach(list,it) for(list_t *it=list_start(list);it!=NULL;it=it->next)
 #endif
 
